c422a64f88c405013b2deea482b5d8d32ac2b37c
bellenker <libertybelle0524.er@gmail.com>
2025-09-19 07:36:15 -0600

    Prep2 challenge complete!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,71 @@
 using System;
+using System.Formats.Asn1;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your overall grade score: ");
+        string letterGrade = Console.ReadLine();
+        int number = int.Parse(letterGrade);
+
+        string sign = "";
+
+        if (number >= 101)
+        {
+            Console.WriteLine("If this is your valid grade score then you have an A, but I would check again to confirm the number.");
+        }
+        else if ((number <= 100) && (number >= 90))
+        {
+            letterGrade = "A";
+        }
+        else if ((number < 90) && (number >= 80))
+        {
+            letterGrade = "B";
+        }
+        else if ((number < 80) && (number >= 70))
+        {
+            letterGrade = "C";
+        }
+        else if ((number < 70) && (number >= 60))
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        int lastDigit = number % 10;
+
+        if (letterGrade != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letterGrade == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"You're grade letter score is: {letterGrade}{sign}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You are passing the course! Awesome job!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, you're not passing the class! Work with teachers and others, you can do it! You just need a C to pass!");
+        }
+
     }
 }
