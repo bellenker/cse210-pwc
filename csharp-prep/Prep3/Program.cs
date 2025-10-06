686b6eb42c099f47a036ac9861880841f4bbde6a
bellenker <libertybelle0524.er@gmail.com>
2025-10-06 11:27:05 -0600

    Finished Prep3 program with stretch challenges.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random number = new Random();
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            int magic = number.Next(1, 101);
+
+            Console.Write("I am thinking of a number between 1 and 100. Can you guess it? ");
+
+            int guess = 0;
+            int guessCount = 0;
+
+            while (guess != magic)
+            {
+                Console.Write("Enter your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+
+                guessCount++;
+
+                if (guess < magic)
+                {
+                    Console.WriteLine("The magic number is higher, try again! ");
+                }
+                else if (guess > magic)
+                {
+                    Console.WriteLine("The magic number is lower, try again! ");
+                }
+                else
+                {
+                    Console.WriteLine("Yep that's right! You guessed it! Great job!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            Console.Write("Would you like to play again? (yes/no): ");
+            string playAgainInput = Console.ReadLine().ToLower();
+            playAgain = (playAgainInput == "yes");
+        }
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
