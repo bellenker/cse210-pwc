9f8d4578008257aad4d56630a09990dda65b8091
bellenker <libertybelle0524.er@gmail.com>
2025-10-06 13:18:50 -0600

    Finished Prep4 program and submitting.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int numbersInput;
+
+        Console.WriteLine("Enter as many numbers as you would like. Enter 0 to stop.");
+
+        do
+        {
+            Console.Write("Please enter a number: ");
+            string addNumber = Console.ReadLine();
+
+            if (int.TryParse(addNumber, out numbersInput))
+            {
+                if (numbersInput != 0)
+                {
+                    numbers.Add(numbersInput);
+                }
+                else
+                {
+                    Console.WriteLine("Thank you for your input. Here are the results!");
+                }
+            }
+        }
+        while (numbersInput != 0);
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+
+
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+        int smallest = int.MaxValue;
+        bool found = false;
+
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < smallest)
+            {
+                smallest = num;
+                found = true;
+            }
+        }
+
+        if (found)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallest}");
+        }
+        else
+        {
+            Console.WriteLine($"No positive numbers were entered.");
+        }
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
