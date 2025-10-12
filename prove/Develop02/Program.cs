413eef2b3eeab59813f5caedfeaeabf49cd16fee
bellenker <libertybelle0524.er@gmail.com>
2025-10-11 22:45:26 -0600

    Updating the Journal program, still working on what isn't working.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        bool run = true;
+
+        while (run)
+        {
+            Console.WriteLine("Journal Menu:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("Please enter your number choice: ");
+
+            string menu = Console.ReadLine();
+
+            switch (menu)
+            {
+                case "1":
+                    journal.Add();
+                    break;
+                case "2":
+                    journal.Display();
+                    break;
+                case "3":
+                    journal.Save();
+                    break;
+                case "4":
+                    journal.Load();
+                    break;
+                case "5":
+                    run = false;
+                    Console.WriteLine("Thank you for your time. Exiting Journal. Have a wonderful day!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+
+            }
+        }
     }
 }
